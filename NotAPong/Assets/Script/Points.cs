f000a752371d810e63be37014b3ec74f50f3d2d8
WMajka <WMajka@users.noreply.github.com>
2020-10-12 19:06:01 +0200

    Almost Complete Pong

--- NotAPong/Assets/Script/Points.cs
+++ NotAPong/Assets/Script/Points.cs
@@ -6,7 +6,8 @@ using TMPro;
 public class Points : MonoBehaviour
 {
     public TextMeshProUGUI[] PlayerTextScore = new TextMeshProUGUI[2];
-    public int[] PlayerPoints = new int[2]; 
+    public int[] PlayerPoints = new int[2];
+    public bool[] PlayerWin = new bool[2];
 
     private void Awake()
     {
@@ -16,6 +17,7 @@ public class Points : MonoBehaviour
     public void AddPoint(int playerNumber)
     {
         PlayerPoints[playerNumber-1]++;
+        SetToWin(playerNumber - 1);
     }
 
     private void LateUpdate()
@@ -27,5 +29,12 @@ public class Points : MonoBehaviour
     {
         PlayerTextScore[0].text = PlayerPoints[0].ToString();
         PlayerTextScore[1].text = PlayerPoints[1].ToString();
+    }
+    public void SetToWin(int playerNumber)
+    {
+        if (PlayerPoints[playerNumber] == 5)
+        {
+            PlayerWin[playerNumber] = true;
+        }
     }
 }
---
+++ NotAPong/Assets/Script/WinCondition.cs
@@ -0,0 +1,33 @@
+﻿using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class WinCondition : MonoBehaviour
+{
+    public Points GetPoints;
+    public TextMeshProUGUI GetText;
+    public GameObject WinScreen;
+
+    private void LateUpdate()
+    {
+        for (int i = 0; i < GetPoints.PlayerWin.Length; i++)
+        {
+            if (GetPoints.PlayerWin[i])
+            {
+                WinScreen.SetActive(true);
+                GetText.text = $"Player {i + 1} Wins";
+                Time.timeScale = 0.0f;
+            }
+        }
+        
+    }
+
+    public void RestartGame()
+    {
+        var activeScene = SceneManager.GetActiveScene().name;
+        SceneManager.LoadScene(activeScene);
+        Time.timeScale = 1.0f;
+        WinScreen.SetActive(false);
+    }
+    
+}
